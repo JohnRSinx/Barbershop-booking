160bede6db6259fcafd54d33dc23593276decc48
John Rodrigues <johnrodrigues97106@gmail.com>
2024-02-01 15:24:23 -0300

    feat:initial barbershop item

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,9 +3,14 @@ import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
+import { Key } from "react";
 
 
-export default function Home() {
+export default async function Home() {
+    // chamar prisma e pegar barbearias
+    const barbershops = await db.barbershop.findMany({});
   return (
     <main >
       <Header/>
@@ -28,6 +33,17 @@ export default function Home() {
       <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
       <BookingItem/>
      </div>
+
+     <div className="mt-6">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop: { id: Key | null | undefined; }) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+      
     </main>
   );
 }
