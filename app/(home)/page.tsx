3e55c28a2009c15517a3767bf8b2c5ba0c15428b
John Rodrigues <johnrodrigues97106@gmail.com>
2024-02-01 11:46:49 -0300

    feat: add title, search home

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,28 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
+
 
 export default function Home() {
   return (
     <main >
       <Header/>
+      <div className="px-5 pt-5">
+        <h2 className="text-x1 font-bold">
+          Olá, Miguel!
+        </h2>
+        <p className="capitalize text-sm">
+        {format(new Date(), "EEEE','dd 'de' MMMM",{
+          locale: ptBR,
+        })}
+      </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search/>
+      </div>
+     
     </main>
   );
 }
