570ef306c4e241f6a9b10324b3daa587123d6187
John Rodrigues <johnrodrigues97106@gmail.com>
2024-02-01 22:22:27 -0300

    feat: add new tables

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import { db } from "@/app/_lib/prisma"
+import { Adapter } from "next-auth/adapters"
+
+const prisma = new PrismaClient()
+
+export default NextAuth({
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId:"" ,
+      clientSecret: "",
+    }),
+  ],
+})
---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,46 @@
+import { db } from "@/app/_lib/prisma";
+import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Key } from "react";
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  };
+}
+
+const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
+  const session = await getServerSession(authOptions);
+
+  if (!params.id) {
+    // TODO: redirecionar para home page
+    return null;
+  }
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+
+  if (!barbershop) {
+    // TODO: redirecionar para home page
+    return null;
+  }
+
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+
+      <div className="px-5 flex flex-col gap-4 py-6">
+        {barbershop.services.map((service: { id: Key | null | undefined; }) => (
+          <ServiceItem key={service.id} barbershop={barbershop} service={service} isAuthenticated={!!session?.user} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BarbershopDetailsPage;
